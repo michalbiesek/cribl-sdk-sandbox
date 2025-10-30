d611588c831ba950205fe440849504c49e8e3e03
Michal Biesek <mbiesek@cribl.io>
2025-10-30 16:45:55 +0100

    Update install script

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -10,27 +10,23 @@ cp .env.example .env
 # Python setup
 echo "🐍 Setting up Python dependencies..."
 pip install --upgrade pip
-pip install --force-reinstall \
-    git+https://github.com/criblio/cribl_control_plane_sdk_python.git \
-    git+https://github.com/criblio/cribl_cloud_management_sdk_python.git
 pip install -r requirements.txt
 
 # Node.js setup
 echo "📦 Setting up Node.js dependencies..."
 npm install
-npm install cribl-control-plane cribl-mgmt-plane
 npm install -g typescript ts-node tsx
 
 # Go setup - Control Plane
 echo "🔧 Setting up Go Control Plane SDK..."
 cd examples/control-plane/go
-go get github.com/criblio/cribl-control-plane-sdk-go
+go mod download
 go mod tidy
 
 # Go setup - Management Plane
 echo "🔧 Setting up Go Management Plane SDK..."
 cd ../../mgmt-plane/go
-go get github.com/criblio/cribl-cloud-management-sdk-go
+go mod download
 go mod tidy
 
 # Return to root
